684b9f2a1fd89b4da79ebd3fef74d84d484a8923
EazyBr33zy <118669376+EazyBr33zy@users.noreply.github.com>
2022-12-06 11:00:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title('Football Data')
 #st.subheader('https://www.football-data.co.uk/')
 
 st.sidebar.header('Ligas')
-selected_league = st.sidebar.selectbox('Ligas', ['Inglaterra','Escócia','alemanha','Itália','Espanha','França','Holanda','Bélgica','Portugal','Turquia','Grécia'])
+selected_league = st.sidebar.selectbox('Ligas', ['Inglaterra','Escócia','Alemanha','Itália','Espanha','França','Holanda','Bélgica','Portugal','Turquia','Grécia'])
 
 st.sidebar.header('Temporada')
 selected_year = st.sidebar.selectbox('Ano', ['2022/2023', '2021/2022', '2020/2021',  '2019/2020',  '2018/2019', '2017/2018',  '2016/2017', '2015/2016', '2014/2015', '2013/2014', '2012/2013', '2011/2012', '2010/2011'])
